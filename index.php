3f1ecd1a9997ac7619a8f7f2ff981a3c326222cf
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-07-28 19:28:12 +0500

    feat: add layouts

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Конфигуратор дверей</title>
-</head>
-<body>
-
-</body>
-</html>
+<?php require_once __DIR__ . '/views/layouts/start.php' ?>
+    <a href="https://t.me/+rgrTsg8CG7AzN2Uy" class="link" target="_blank">Наш телеграм-канал</a>
+    <div class="wrapper mx-5 my-2">
+        <h1 class="text-center mb-4">Конфигуратор входной двери</h1>
+        <main class="main">
+            <section class="main__doors">
+                <div class="main__doors-item" id="door1">
+                    <span class="main__doors-handle" id="handle-front"></span>
+                </div>
+                <div class="main__doors-item" id="door2">
+                    <span class="main__doors-handle" id="handle-back"></span>
+                </div>
+            </section>
+            <section class="main__params">
+                <h4 class="main__params-title">Параметры</h4>
+                <?php require_once __DIR__ . '/views/components/form.php' ?>
+            </section>
+            <section class="main__list">
+                <h4 class="main__list-title">Последние заказы</h4>
+                <?php require_once __DIR__ . '/views/components/list.php'; ?>
+            </section>
+        </main>
+    </div>
+<?php require_once __DIR__ . '/views/layouts/end.php' ?>
---
+++ views/layouts/start.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="./assets/js/index.js"></script>
+    <link rel="icon" href="./assets/img/favicon.ico">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="./assets/styles/index.css">
+    <title>Конфигуратор дверей</title>
+</head>
+<body>
