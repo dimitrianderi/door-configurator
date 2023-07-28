e306aa9b44b4daf1b062012706994501269d40c1
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-07-28 20:41:16 +0500

    fix: correct config

--- controllers/prepare_config.php
+++ controllers/prepare_config.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../database/connect.php';
 
 $sql = "SELECT * FROM `config`";
 
-$config = $pdo->query($sql)->fetchAll();
+$config = $pdo->query($sql)->fetchAll()[0];
 
 $token = $config['token'];
 $chat_id = $config['chat_id'];
